625e6f2087cc769ee0b696c2d4a7f4d0a27c169d
Sunny Do <sunnydo@Sunnys-iMac.local>
2018-01-18 19:20:08 +0800

    Init

---
+++ SpeedTest/ViewController/NewsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewsViewController.swift
+//  SpeedTest
+//
+//  Created by Sunny Do on 18/01/2018.
+//  Copyright © 2018 Sunny Do. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
