46ffe2cc976a190d091386176f542c6293b61c99
Sunny Do <sunnydo888@gmail.com>
2018-01-18 15:06:56 +0800

    Initial Commit

---
+++ SpeedTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeedTest
+//
+//  Created by Sunny Do on 18/01/2018.
+//  Copyright © 2018 Sunny Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
